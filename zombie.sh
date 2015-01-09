915759f422a115a58c4013c374fae028a62a7535
Emanz93 <emanuele.lovera@gmail.com>
2015-01-09 10:48:41 +0100

    Update zombie.sh
    Now the script should start even if there are others $process running. It'll able to count correctly the number of $process's zombie.

--- zombie.sh
+++ zombie.sh
@@ -6,24 +6,25 @@
 echo "Please enter the name of the process to be monitored:"
 read process #variable that contains the name of the process
 
+c_p=`ps aux | grep $process | wc -l` #partial counter of process
 let "c_process = 0" #counter of number of 'process'
 
 #loop util i'll find 'process'
-while [ $c_process -lt 1 ]
+while [ $c_process -le $c_p ]
 do
-	c_process=`ps aux | grep root | grep $process | wc -l`
+	c_process=`ps aux | grep $process | wc -l`
 done
 
 let "c_past = 0" #Number of zombie in previous-loop.
 let "c_now = 0" #Number of zombie in this loop.
 let "zombie = 0" #Number of real zombie.
-while [ $c_process -ge 1 ]
+while [ $c_process -gt $c_p ]
 do
 	#Update the number(s) of process
-	c_process=`ps aux | grep root | grep $process | wc -l`
+	c_process=`ps aux | grep $process | wc -l`
 
 	#Count the number of zombie process in this moment
-	c_now=`ps aux | awk '{ print $8 " " $2 }' | grep -w Z | wc -l`
+	c_now=`ps aux | grep -w Z | grep -w $process | wc -l`
 
 	#calculating the difference because i'll increase the number of Zombie only if there's an increase in the
 	#difference between c_pat and c_now
@@ -31,6 +32,7 @@ do
 	if [ $difference -gt 0 ]; then
 		#I'll count if difference is more than 0. count real number of zombie.
 		zombie=`expr $zombie + 1`
+		echo "zombie!"
 	fi
 
 	#Update the c_past for the next loop
