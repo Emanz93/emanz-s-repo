d4bd172696a4f7d7065119bd9f2e18066b7d2433
Emanz93 <emanuele.lovera@gmail.com>
2015-01-16 12:10:16 +0100

    Update Zombie.sh
    Bug fix. Correct detecting of $process's name.

--- zombie.sh
+++ zombie.sh
@@ -2,31 +2,34 @@
 
 #This script allow to count number of zombies process will be create during the execution of a process.
 #The script will terminate when the process will die.
+#Author Emanz93 - Emanuele Lovera
 
 echo "Please enter the name of the process to be monitored:"
 read process #variable that contains the name of the process
 
 logfile="zombie_$process.log" #Name of the logfile
 
-c_p=`ps aux | grep $process | wc -l` #partial counter of process
+#c_p=`ps awx | awk '{ print $3 " " $5 }' | grep $process | wc -l` #partial counter of process
 let "c_process = 0" #counter of number of 'process'
 
-#loop util i'll find 'process'
-while [ $c_process -le $c_p ]
+#loop util i'll find '$process'
+while [ $c_process -le 0 ]
 do
-	c_process=`ps aux | grep $process | wc -l`
+	c_process=`ps awx | awk '{ print $3 " " $5 }' | grep $process | wc -l`
+	echo "c_process = $c_process"
 done
 
 let "c_past = 0" #Number of zombie in previous-loop.
 let "c_now = 0" #Number of zombie in this loop.
 let "zombie = 0" #Number of real zombie.
-while [ $c_process -gt $c_p ]
+
+while [ $c_process -gt 1 ]
 do
 	#Update the number(s) of process
-	c_process=`ps aux | grep $process | wc -l`
-
+	c_process=`ps awx | awk '{ print $3 " " $5 }' | grep $process | wc -l`
+	echo "c_process = $c_process"
 	#Count the number of zombie process in this moment
-	c_now=`ps aux | grep -w Z | grep -w $process | wc -l`
+	c_now=`ps awx | awk '{ print $3 " " $5 }' | grep $process | grep Z | wc -l`
 
 	#calculating the difference because i'll increase the number of Zombie only if there's an increase in the
 	#difference between c_pat and c_now
